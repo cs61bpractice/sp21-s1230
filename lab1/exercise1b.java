9e98e8b4520bf0fc49a802abd4ba363be6a48070
piziyi <piziyi007@gmail.com>
2023-08-30 19:34:12 +0800

    exercise1b finished, the previous commit was not added yet

--- lab1/exercise1b.java
+++ lab1/exercise1b.java
@@ -1,2 +1,15 @@
 public class exercise1b {
+    public static void drawTriangle(int N) {
+        for (int i = 1; i <= N; i ++) {
+            int col = 1;
+            while (col < i) {
+                System.out.print("x");
+                col += 1;
+            }
+            System.out.println("x");
+        }
+    }
+    public static void main(String[] args) {
+        drawTriangle(10);
+    }
 }
